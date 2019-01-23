8c28d7e68ccd52bd913c03c84f5bd8d50365d708
TREINAR <TREINAR@172.16.99.167>
2019-01-23 21:36:33 -0200

    Verdade

--- reginaldo/treinar/src/br/com/treinarminas/academico/PrimeiraClasse.java
+++ reginaldo/treinar/src/br/com/treinarminas/academico/PrimeiraClasse.java
@@ -4,7 +4,7 @@ import javax.swing.event.SwingPropertyChangeSupport;
 
 public class PrimeiraClasse {
 	public static void main(String[] args) {
-		System.out.println("Márcio");
+		System.out.println("Reginaldo");
 	}
 
 }
