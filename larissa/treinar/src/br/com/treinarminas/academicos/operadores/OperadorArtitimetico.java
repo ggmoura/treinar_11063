c62d6e5230b4a76d9a578d29f6f1ace65d25232d
TREINAR <TREINAR@172.16.99.145>
2019-01-23 21:20:39 -0200

    Estou começando um projeto

---
+++ larissa/treinar/src/br/com/treinarminas/academicos/operadores/OperadorArtitimetico.java
@@ -0,0 +1,48 @@
+package br.com.treinarminas.academicos.operadores;
+
+public class OperadorArtitimetico {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int soma = a + b;
+		System.out.println("A Soma de " + a + " + "  + b + " = " + soma);
+		
+		
+		int c = 20; 
+		int d = 10;
+		int subtracao = c - d;
+		System.out.println("A Subtração de " + c + " - " + d + " = " + subtracao);
+		
+		int e = 2; 
+		int f = 20;
+		int mult = e * f;
+		System.out.println("A Multiplicação de " + e + " * " + f + " = " + mult);
+		
+		int g = 20; 
+		int h = 2;
+		int div = g / h;
+		System.out.println("A Divisão de " + g + " / " + h + " = " + div);
+		
+		int i = 20; 
+		int j = 2;
+		int mod = i % j;
+		System.out.println("A Divisão de " + i + " % " + j + " = " + mod);
+		
+		
+	
+		
+		
+	}
+	
+	
+		
+	
+	
+	
+				
+				
+	
+			
+
+}
---
+++ larissa/treinar/src/br/com/treinarminas/acadmico/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package br.com.treinarminas.acadmico;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.println("Larissa");
+	}
+	
+}
