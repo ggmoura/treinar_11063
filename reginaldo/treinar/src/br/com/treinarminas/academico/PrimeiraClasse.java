09ee08a3e66f3e7ef8f4ce85fef3d2dc926cb99b
Brenocardoso <mbrenoskt@hotmail.com>
2019-01-23 21:33:59 -0200

    ignore

--- reginaldo/treinar/src/br/com/treinarminas/academico/PrimeiraClasse.java
+++ reginaldo/treinar/src/br/com/treinarminas/academico/PrimeiraClasse.java
@@ -4,7 +4,7 @@ import javax.swing.event.SwingPropertyChangeSupport;
 
 public class PrimeiraClasse {
 	public static void main(String[] args) {
-		System.out.println("Reginaldo");
+		System.out.println("Márcio");
 	}
 
 }
