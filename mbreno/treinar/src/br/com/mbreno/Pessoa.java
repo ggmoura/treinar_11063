745cde671d67285fad9289473bdc3f46edc46ae9
Treinar <Treinar@SL03PROFESSOR>
2019-01-23 22:21:09 -0200

    teste

--- mbreno/treinar/src/br/com/mbreno/Pessoa.java
+++
@@ -1,9 +0,0 @@
-package br.com.mbreno;
-
-public class Pessoa {
-	
-	int idade;
-	char sexo;
-	String nome;
-	
-}
---
+++ mbreno/treinar/src/br/com/mbreno/academicos/Atividades.java
@@ -0,0 +1,7 @@
+package br.com.mbreno.academicos;
+
+public class Atividades {
+	public static void main(String[] args) {
+		System.out.println("Márcio Breno");
+	}
+}
