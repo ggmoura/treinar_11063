6916970ec57216ddbb432a8bdefd69f148bb0951
Eclipse <Eclipse@DESKTOP-IML51AA.home>
2019-01-24 17:12:55 -0200

    teste

--- fabio/treinar/src/br/com/treinarminas/academico/Decision/ComandoIf.java
+++ fabio/treinar/src/br/com/treinarminas/academico/Decision/ComandoIf.java
@@ -14,7 +14,7 @@ public class ComandoIf {
 				}
 			}
 			System.out.println("Fim do programa!");
-			System.out.println("Fim do programa12!");
+			System.out.println("Fim do programa1!");
 			
 		}
 	}
