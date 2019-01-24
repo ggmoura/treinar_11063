e4dfa21e9b6271b5261b17fd0476898bf96ae727
fabiocardosoti <fabiocardosoti@gmail.com>
2019-01-24 12:33:35 -0300

    Update PrimeiraClasse.java

--- fabio/treinar/src/br/com/treinarminas/academico/PrimeiraClasse.java
+++ fabio/treinar/src/br/com/treinarminas/academico/PrimeiraClasse.java
@@ -3,5 +3,6 @@ package br.com.treinarminas.academico;
 public class PrimeiraClasse {
 	public static void main(String[] args) {
 		System.out.println("teste de classe");
+		System.out.println("teste de git");
 	}
 }
