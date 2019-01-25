6e039303c6bcefde3021185af0f2c0eb41150c4a
Treinar <Treinar@SL03PROFESSOR>
2019-01-25 21:15:45 -0200

    Projeto inter

---
+++ professor/inter/src/br/com/inter/banco/modelo/core/Cliente.java
@@ -0,0 +1,24 @@
+package br.com.inter.banco.modelo.core;
+
+public class Cliente {
+
+	private String nomeCliente;
+	private Long cpf;
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+}
---
+++ professor/inter/src/br/com/inter/banco/visao/TelaMenu.java
@@ -0,0 +1,54 @@
+package br.com.inter.banco.visao;
+
+import java.util.Scanner;
+
+import br.com.inter.banco.controle.BancoControle;
+
+public class TelaMenu {
+
+	private BancoControle controle;
+	private Scanner leitor;
+	
+	public TelaMenu() {
+		controle = new BancoControle();
+		leitor = new Scanner(System.in);
+	}
+	
+	public void iniciarMenu() {
+		System.out.print(recuperarMenu());
+		Integer opcao = leitor.nextInt();
+		leitor.nextLine();
+		while (opcao != 0) {
+			switch (opcao) {
+			case 1:
+				criarConta();
+				break;
+
+			default:
+				System.out.println("Opção inválida!");
+				break;
+			}
+			System.out.print(recuperarMenu());
+			opcao = leitor.nextInt();
+		}
+		leitor.close();
+	}
+	
+	private void criarConta() {
+		System.out.print("Informe o nome do Cliente: ");
+		String nomeCliente = leitor.nextLine();
+		System.out.print("Informe o CPF do Cliente: ");
+		Long cpf = leitor.nextLong();
+		System.out.print("Informe o número da Conta: ");
+		Integer numeroConta = leitor.nextInt();
+		controle.criarConta(nomeCliente, numeroConta, cpf);
+		System.out.println("\nConta criada com sucesso!\n");
+	}
+
+	private String recuperarMenu() {
+		return "Informe:\n\t"
+				+ "1 - Criar Conta\n\t"
+				+ "0 - Sair\n\n==> ";
+	}
+	
+}
