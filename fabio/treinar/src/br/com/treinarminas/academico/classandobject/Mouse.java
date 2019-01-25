34ec56f4e379737cac18c4886792aac288e85789
Eclipse <Eclipse@DESKTOP-IML51AA.home>
2019-01-24 22:15:04 -0200

    criacao metodos

--- fabio/treinar/src/br/com/treinarminas/academico/classandobject/Mouse.java
+++ fabio/treinar/src/br/com/treinarminas/academico/classandobject/Mouse.java
@@ -6,4 +6,27 @@ public class Mouse {
 	String cor;
 	int numBotoes;
 	Fabricante fabricante;
+	int clkBtnBireito;
+	int clkBtnEsquerdo;
+	
+	// Regra clicarBtnDireito retrorna inteiro 1;
+	// Regra clicarBtnEsquedo retrorna inteiro 2;
+	// Regra retorna quantidade clcks botao direito 
+	// Regra retorna quantidade clcks botao esquerdo 
+
+	void clicarBtnDireito(){
+		clkBtnBireito +=1;
+	}
+	void clicarBtnEsquerdo(){
+		clkBtnEsquerdo +=1;
+	}
+	int retornaQtdclickBtndireito(){
+		return clkBtnBireito;
+	}
+	
+	int retornaQtdclickBtnEsquerdo(){
+		return clkBtnEsquerdo;
+	}
+	
+ 
 }
