27365a7b2ba9ff62fe22894c131cf7478dd1800f
Treinar <Treinar@MICRO>
2019-02-11 21:15:58 -0200

    Atividade maoi

---
+++ marcus/treinar/src/br/com/treinar/academico/TesteMapMaoi.java
@@ -0,0 +1,27 @@
+package br.com.treinar.academico;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TesteMapMaoi {
+	public static void main(String[] args) {
+
+		List<String> estringue = Arrays.asList("lalalala", "mah oee", "aiai hihi", "roque!", "vai pra la",
+				"quem é q eu vo chamá");
+		// System.out.println(strings);
+		Map<Character, List<String>> nomesPessoa = new HashMap<>();
+
+		for (String nome : estringue) {
+			Character primeiraLetra = nome.charAt(0);
+			// System.out.println(primeiraLetra);
+			if (nomesPessoa.containsKey(primeiraLetra)) {
+				nomesPessoa.put(primeiraLetra, new ArrayList<>());
+			}
+			nomesPessoa.get(primeiraLetra).add(nome);
+		}
+		System.out.println(nomesPessoa);
+	}
+}
