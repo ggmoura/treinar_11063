6feecf692fff883fc4d3619eca6f59396d815515
Eclipse <Eclipse@vmjavaFabio.home>
2019-02-21 18:57:32 -0300

    servlet

---
+++ treinar-web/src/main/java/br/com/trienar/web/servlet/PrimeiroServlet.java
@@ -0,0 +1,30 @@
+package br.com.trienar.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimeiroServlet
+ */
+@WebServlet("/PrimeiroServlet")
+public class PrimeiroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PrimeiroServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("<h1>Olá Mundo Java<h1>");
+	}
+
+
+}
