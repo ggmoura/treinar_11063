a80b16e6106c85d701b062b021a767fc042b43c7
andreffs <andure@bol.com.br>
2019-02-21 19:48:22 -0300

    Project Initiation

---
+++ bethuel/treinar-web/src/main/java/br/com/treinar/web/servlet/PrimeiroServlet.java
@@ -0,0 +1,32 @@
+package br.com.treinar.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimeiroServlet
+ */
+@WebServlet("/PrimeiroServlet")
+public class PrimeiroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public PrimeiroServlet() {
+    	System.out.println("Abriu a página");
+    	
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
