005e9734aeb38b8985c7e5755858a6ee6b967c6e
Eclipse <Eclipse@192.168.25.138>
2019-02-22 19:27:23 -0300

    Carregar treinar web professor

--- treinar-web/src/main/java/br/com/treinar/web/servlet/PrimeiroServlet.java
+++ treinar-web/src/main/java/br/com/treinar/web/servlet/PrimeiroServlet.java
@@ -1,30 +1,46 @@
 package br.com.treinar.web.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//Caso nome classe igual Servlet
+//@WebServlet("TerceiroServlet")
+//Caso nome servlet diferente, neste caso um array
+//@WebServlet(urlPatterns= {"*.reginaldo", "casodois"})
+
 @WebServlet("/PrimeiroServlet")
 public class PrimeiroServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public PrimeiroServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public PrimeiroServlet() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String nome = request.getParameter("nomeDaPessoa");
+		String idade = request.getParameter("idadeDaPessoa");
+		String msg = "Olá ".concat(nome).concat(" Parabéns pelos seus ").concat(idade);
+		
+		java.util.List<String> nomes = new ArrayList<String>();
+		nomes.add("reginaldo");
+		nomes.add("vanessa");
+		nomes.add("beatriz");
+		nomes.add("emanuela");
+		
+		request.setAttribute("parabens", nomes);
+		request.getRequestDispatcher("/cadastro.jsp").forward(request, response);
 	}
-
 }
 //Java bin - tem get e set para todos as atributos
-
